6f2c7e8ed84c2d86d91b7f8056477c5df4f760a9
Jazzy <jahanzaib8845@gmail.com>
2025-04-15 19:57:33 +0500

    Tree JS project setup

---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+import { meta, shopify, starbucks, tesla, logo } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Fullstack Developer",
+        company_name: "Enigmatix Global",
+        icon: logo,
+        iconBg: "#accbe1",
+        date: "Jun 2024 - Present",
+        type: "Remote",
+        points: [
+            "Developing and maintaining fullstack applications using React.js, Nest.js, MongoDB, Node.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Fullstack Developer",
+        company_name: "Wosom Service Limited",
+        icon: logo,
+        iconBg: "#fbc3bc",
+        date: "Jan 2024 - Jun 2024",
+        type: "Remote",
+        points: [
+            "Developing and maintaining fullstack applications using React.js, Django, DRF, Postgres and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Sr. Frontend Developer",
+        company_name: "TrioSoft",
+        icon: logo,
+        iconBg: "#b7e4c7",
+        date: "Jul 2023 - Dec 2023",
+        type: "Remote",
+        points: [
+            "Developing and maintaining web applications using React.js, Vue.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Sr. Mobile App Developer",
+        company_name: "Pragmatech",
+        icon: logo,
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Jul 2023",
+        type: "Remote",
+        points: [
+            "Developing and maintaining mobile applications using React Native and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Sr. Frontend Developer",
+        company_name: "Enigmatix",
+        icon: logo,
+        iconBg: "#a2d2ff",
+        date: "Jan 2020 - Jan 2023",
+        type: "Onsite",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Wosom Event Managment',
+        description: 'Developed a web application that tracks and manage events of any kinde for any kind of users, also helping clients find the best deals.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'RentArround E-Commerce',
+        description: 'Created a full-stack system of the E-Commerce platform," enabling users to buy, rent and purchase in anything anywhere.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'LMS AI Management',
+        description: 'Built a complete schooling managemnet system powerd by AI, allowing students to mange activites and connect with teachers and also can use AI tools.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Parental Controle Application',
+        description: 'Developed a mobile application for parental control, facilitating parents to searches and keep an eye on their childs.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Fitness Application',
+        description: 'This Fitness Mobile Application is a dynamic platform designed to help users achieve their health and fitness goals.',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Freelance Marketplace',
+        description: "As a developer on this project, my primary role was to ensure the platform's efficiency and reliability in connecting businesses with experts.",
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'E-Marketplace',
+        description: 'In this E-Commerce Marketplace application, my primary role was to design and develop a scalable, user-centric platform that streamlines buying and selling for both customers and merchants',
+        link: 'https://github.com/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI SEO Marketplace',
+        description: 'This web Application is a dynamic platform designed to help websites SEO make better on advance level with the help of AI.',
+        link: 'https://github.com/',
+    }
+];
